0183d4f5251950ace7c2dcaf794385c77187a694
QuiqueMoyaPerez <al449327@uji.es>
2026-09-23 17:00:51 +0200

    dar la vuelta task 1

--- Teleoperation/scripts/ros_bag_play.sh
+++ Teleoperation/scripts/ros_bag_play.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
-export ROS_DOMAIN_ID=2
-ros2 bag play rosbag2_2026_09_16-16_15_14
+export ROS_LOCALHOST_ONLY=1
+ros2 bag play rosbag2_2026_09_23-16_26_47
--- Teleoperation/scripts/ros_bag_record.sh
+++ Teleoperation/scripts/ros_bag_record.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
-export ROS_DOMAIN_ID=2
+export ROS_LOCALHOST_ONLY=1
 export TURTLEBOT3_MODEL=burger
-ros2 bag record /clock /odom /tf /tf_static
+ros2 bag record /clock /odom /tf /tf_static /scan /map
 
